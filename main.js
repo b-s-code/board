f36518d01d7773e1f5d716892952441b282c230d
b-s-code <89502375+b-s-code@users.noreply.github.com>
2023-05-10 13:47:55 +0800

    make window

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello from electron');
+const {app /* controls app's event lifecycle*/, BrowserWindow /* manages app windows*/ } = require('electron');
+
+const createWindow = () =>
+{
+    const win = new BrowserWindow
+    ({
+        width: 800,
+        height: 600,
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {createWindow()});
