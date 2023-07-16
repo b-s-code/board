0f1b88f009cc6ea6aab92de6a558072135086b15
b-s-code <89502375+b-s-code@users.noreply.github.com>
2023-07-16 15:59:41 +0800

    more sketching

--- new_architecture_idea.md
+++ new_architecture_idea.md
@@ -6,7 +6,7 @@ Makes it less than joyous to work with.
 ## DATA MODEL
 
 ```pseudocode
-struct DataModel
+struct BoardStateModel
 {
     /* CARDS */
     // Do not have a reference to the list they belong to.
@@ -20,28 +20,60 @@ struct DataModel
     uint[]          listsIds            // Used when anything needs a reference to a list.  Allows duplicate list titles.
     string[]        listTitles
     (uint[])[]      listsCards          // Tracks the card-list relation.  Order of cardIds within an arr elt specifies top-to-bottom order in GUI.
-
-    /* GUI */
-    string          guiViewMode         // "welcome", "aggregate", "focused card", or "download data".  "welcome" view should have load/new buttons.
     uint[]          listsPositions      // From LHS.  Parallel to listIds.
 }
 ```
 
+```pseudocode
+struct AppStateModel
+{
+    /* GUI */
+    string          guiViewMode         // "welcome", "aggregate", "focused", or "download data".  "welcome" view should have load/new buttons.
+    uint            focusedCardId
+}
+```
+
+
 ## WHAT SHOULD THE CODE LOOK LIKE?
 
-A global state object for the data model.
+### Data
 
-Rendering functions for each guiViewMode.
+Two global state objects:
 
-A bunch of pure functions where you pass in global state and return a new global state.
-(It's not that much data.)
+1. Application state.  Created at start of session, discarded at the end.
+2. Board state.  Loaded/saved as file, read/written to at runtime.
 
-The rendering functions should set up EventListeners for GUI controls.
-Clicking on stuff, changing the text in a text field etc will trigger events.
-The event handlers will:
-- call one of the pure functions
-    - passing in copy of current global state
-    - passing in new value to be assigned to something in the data model
-    - new global state will be set using return value
-- call an appropriate rendering function
+### Functions
 
+Basic flow is:
+
+1. App gets initialized.
+2. The following occur in a loop:
+
+    - User perfoms an action.
+    - Action triggers an event.
+    - Event handler calls a pure function, passing board state and required data to make user's requested change.
+    - Event handler assigns the return value of the pure function to the global board state.
+    - Board state, but not necessarily GUI now represents what user wants.
+    - Event handler makes any necessary changes to application state, raises an outdated GUI event then returns.
+    - Outdated GUI event handler calls appropriate render function.
+    - Render function deletes a bunch of stuff from the DOM, reads from app state and board state, draws everything and sets up event handlers to respond to user's next action.
+
+So what code to write?
+
+We'll have a rendering function for each guiViewMode.
+Each should be void, take no params, only read from (never write to) application and board states, and should affect the DOM.
+Each is responsible for creating event handlers for all and only possible user actions that may be taken for that rendered view.
+Any modification of board and application state should be left to event handlers, and they'll accomplish the board state modification by calling pure functions and assigning the return value to global board state.  They can modify application state themselves though.
+
+Also need functions for:
+
+- loading board state from file
+- exporting board state to file and initiating download
+- constructing a default board state
+
+Other thoughts...
+
+The initialization function will have to:
+
+- call either the board state constructing function or the board state loading function, depending on user choice
--- script.js
+++ script.js
@@ -1,257 +1,196 @@
 /*
-* Holds state of the user's board.  Can be imported and exported as file.
+* Holds state of the user's board.
+* Can be imported and exported as file.
+* Can be auto-populated as a blank canvas for  new board.
 */
-var boardData = '{}';
-
-const uiOutdatedEvent = new Event("uiNeedsUpdate");
-const fileSelector = document.getElementById("fileInputControlHidden");
-const saveControls = document.getElementById("saveControls");
-const fileExportControl = document.getElementById("fileExportControl");
-const listContainer = document.getElementById("listContainer");
-const modalCloseButtons = document.getElementsByClassName("modalCloseButton");
+var boardState = {};
 
 /*
-* Updates UI from global boardData state, which is assumed to be up to date.
+* Will not be imported or exported.
+* Intended guiViewMode space is "welcome", "aggregate", "focused", "download".
 */
-document.addEventListener(
-    "uiNeedsUpdate",
-    (e) =>
-    {
-        const userLists = boardData.lists;
-        const userCards = boardData.cards;
-        const listIds = Object.keys(userLists);
-        
-        // Empty container before filling it.
-        let listNodes = document.getElementsByClassName("userList");
-        const oldNumUserLists = listNodes.length;
-        for (let i = 0; i < oldNumUserLists; i++)
-        {
-            listNodes[0].remove();
-        }; 
-        
-        // TODO : change loop over list id's because it doesn't respect position property
-        // instead, could have a dict of { position : id } and loop over keys, looking up id
-        // as needed to get data for that list.
-        // Alternatively, could still loop over id's, but do some positoning logic...
+var appState = {guiViewMode = "welcome", focusedCardId = 0};
 
-        listIds.forEach(id =>
-        {
-            // Get data needed to build the div for the new list. 
-            const newListDiv = document.createElement("div");
-            const theList = userLists[id];
-            const listTitle = document.createTextNode(id.toString() + " : " + theList.name);
-            const postitionProperty = document.createTextNode("position property : " +    theList.position.toString());
-            const br = document.createElement("br");
-
-            // Build the div required for the new list.
-            newListDiv.className = "userList";
-            newListDiv.appendChild(listTitle);
-            newListDiv.appendChild(br);
-            newListDiv.appendChild(postitionProperty);
-
-            // Add each relevant card to the list, along with its ID number.
-            arrCardsForThisList = userLists[id].cardIds.map(i => [i, userCards[i]]);
-            arrCardsForThisList.forEach(AddCardNode);
-            
-            // Helper function.
-            function AddCardNode(cardParts)
-            {
-                // Get data needed to build the div for the new card.
-                // The index of this card in the global state's card array is used so that the div
-                // can know about its backing data structure.
-                const cardId = cardParts[0];
-                const card = cardParts[1];
-                const cardTitle = document.createTextNode(card.title);
-                
-                // Build the div required for the new card.
-                const newCardDiv = document.createElement("div");
-                newCardDiv.className = "userCard rectDropButton";
-                newCardDiv.id = "cardNumber" + cardId.toString();
-                newCardDiv.appendChild(cardTitle);
-               
-                // The card's labels need to be shown too.
-                newCardDiv.appendChild(RenderedLabels(cardId));
-
-                // Each card needs an event listener to display the same (shared) modal when clicked.
-                // No need to clean these up manually, the listeners die
-                // when the card element gets garbage collected.
-                newCardDiv.addEventListener("click", (e) =>
-                {
-                    // Modal should be shown to focus a card's details when a card is clicked.
-                    document.getElementById("modalCardContainer").style.display = "flex";
-
-                    // Delete full card div if and only if one already exists.
-                    let fullCards = document.getElementsByClassName("fullCard");
-                    if (fullCards.length > 0)
-                    {
-                        fullCards[0].remove();
-                    }
-                    
-                    // Inject div for card that user has clicked.
-                    document.getElementById("modalCardContent").appendChild(FullCard(cardId));
-                });
-
-                // Card div is good to go.
-                newListDiv.appendChild(newCardDiv);
-            }
-            
-            // List div is good to go.
-            listContainer.appendChild(newListDiv);
-        });
-
-        // Now must set number of visual columns in list container.
-        // Could add 1 if, for example, placing a "new list" button to the right of the rightmost user list.
-        const newNumCols = listIds.length;
-        listContainer.style.gridTemplateColumns = " auto ".repeat(newNumCols);
-    },
-    false
-);
+/* TODO : sketch out events, handlers, initialization function */
 
 /*
-* Import board data from user-specified file.
-* Raise an event when (successfully) finished to get the UI updated.
-* No need for user to manually "load" a file, once selected.
+* Caller should set guiViewMode, then call this, which will call other
+* rendering functions as needed.
+* Why not call them directly?  This function takes responsibility for 
+* ensuring they're called in correct order.
 */
-fileSelector.onchange = (e) =>
+function Render()
 {
-    const file = fileSelector.files[0]
-    if (!file)
-    {
-        return;
-    }
-
-    const reader = new FileReader();
-    reader.onload = (e) =>
-    {
-        // e.target points to the reader
-        const textContent = e.target.result;
-        boardData = JSON.parse(textContent);
-        document.dispatchEvent(uiOutdatedEvent);
-    }
-    reader.onerror = (e) =>
-    {
-        const error = e.target.error;
-        console.error(`Error occured while reading ${file.name}`, error);
-    }
-
-    reader.readAsText(file);
-};
-
-/*
-* Export board data to JSON for user to download.
-*/
-fileExportControl.addEventListener("click", (e) =>
-{
-    const exportData = new Blob([JSON.stringify(boardData)], {type: 'application/json'});
-    const downloadURL = window.URL.createObjectURL(exportData);
-    document.getElementById("downloadLink").href = downloadURL;
-    document.getElementById("modalDownloadContainer").style.display = "block";
-});
-
-/*
-* Download controls not needed once download starts.
-*/
-document.getElementById("downloadLink").addEventListener("click", (e) =>
-{
-    document.getElementById("modalDownloadContainer").style.display = "none";
-});
-
-/*
-* User should be able to close any modal.
-* Can get away with only running this once because all user cards share the same modal.
-*/
-[...modalCloseButtons].forEach(closeBtn => 
-{
-    closeBtn.addEventListener("click", function (e) 
-    {
-        // User may wish to navigate away from looking at the modal for a card.      
-        this.parentNode.parentNode.style.display = "none";
-    });
-});
-
-/*
-* Returns a div containing most of the data available about the card with
-* the supplied card ID.  Not just the summary information that will be 
-* displayed when the cards are viewed in aggregate.  But everything that 
-* should be shown when this card is in focus.
-*/
-function FullCard(cardId)
-{
-    // The data source for building the GUI full card.
-    const card = boardData.cards[cardId];
-    
-    let outputDiv = document.createElement("div");
-    outputDiv.className = "fullCard";
-    const titleDiv = document.createElement("div");
-    const titleText = document.createTextNode(card.title);
-    const labelDiv = RenderedLabels(cardId, true);
-    const notesText = document.createTextNode(card.notes);
-    titleDiv.appendChild(titleText);
-    titleDiv.style.fontSize = "1.5em";
-    titleDiv.style.fontWeight = "bold";
-    titleDiv.style.padding = "5px 0px 5px 0";
-    outputDiv.appendChild(labelDiv);
-    outputDiv.appendChild(titleDiv);
-    outputDiv.appendChild(notesText);
-    // TODO : deploy more data from card to modal content.
-    // TODO : make modal full card interative.
-
-    return outputDiv; 
+    // if guiViewMode is "focused card", the aggregate view needs
+    // to be rendered first to ensure background is up to date
 }
 
 /*
-* Returns a div containing the rendered labels of the card with supplied id.
-* Optionally styles label container div differently and add's ability for user
-* to add remove labels.  Pass true for isCardInFocus for this behaviour.
+* Renders the welcome screen, where user can create new board or load file.
 */
-function RenderedLabels(cardId, isCardInFocus = false)
+function RenderWelcome()
 {
-    // The data source for building the labels.
-    const card = boardData.cards[cardId];
-    const cardLabels = card.attributes.labels;
+    //
+}
 
-    const newLabelContainerDiv = document.createElement("div");
-    newLabelContainerDiv.className = "labelContainer";
-    newLabelContainerDiv.style.gridTemplateColumns = " auto ".repeat(cardLabels.length + isCardInFocus);
+/*
+* Renders the aggregate view.
+* Shows lists, cards
+* Provides controls for saving boardData to file,
+* adding/deleting/repositioning cards/lists,
+* renaming lists,
+* bringing one card into focus.
+*/
+function RenderAggregate()
+{
+    //
+}
 
-    // Helper function.  If a user tries to make a label labelled "+",
-    // they will encounter confusing behaviour, this string is reserved
-    // for the UI control for adding new labels.
-    function MakeLabel(isCardInFocus, label)
-    {
-        const newLabelDiv = document.createElement("div");
-        newLabelDiv.appendChild(document.createTextNode(label));
-        newLabelDiv.classList.add("label");
+/*
+* Renders a view of one focused card.
+* Shows title, labels, notes.
+* Provides controls for renaming card,
+* changing notes, adding/deleting labels
+*/
+function RenderFocus()
+{
+    //
+}
 
-        // We don't want labels to be interactive when user is in
-        // aggregate view of all lists.
-        if (isCardInFocus)
-        {
-            // TODO : add actual interactivity for adding deleting labels,
-            // currently just have gui hover effect.
-            if (label != "+")
-            {
-                newLabelDiv.classList.add("labelOnFocusedCard");
-            }
-            else
-            {
-                newLabelDiv.classList.add("addLabelControlOnFocusedCard");
-            }
-        }
-    
-        newLabelContainerDiv.appendChild(newLabelDiv);
-    }
+/*
+* Called when user clicks button to download their board data.
+* Serializes boardData to a JSON text file and initiates download.
+*/
+function ExportBoard(boardData)
+{
+    //
+}
 
-    let labelArr = cardLabels;
-    // We don't want the control for adding labels in aggregate view.
-    if (isCardInFocus)
-    {
-        labelArr.push("+");
-    }
-    labelArr.forEach(label =>
-    {
-        MakeLabel(isCardInFocus, label)
-    });
+/*
+* Called when user clicks button to load existing board data from file.
+*/
+function LoadBoard()
+{
+    //
+}
 
-    return newLabelContainerDiv;
+/*
+* Returns a relatively empty board data object which can be assigned to boardData.
+* Called when user clicks button to create a new board.
+*/
+function CreateBoard()
+{
+    //
+}
+
+/*
+* Pure function.
+* Returns a new board, with card moved one unit in direction dir.
+* Return input board if dir is not possible direction movement,
+* given card's current position.
+* dir can be "up", "down", "left", or "right".
+*/
+function MoveCard(board, cardId, dir)
+{
+    //
+}
+
+/*
+* Pure function.
+* Returns a new board, with list moved one unit in direction dir.
+* Return input board if dir is not possible direction movement,
+* given list's current position.
+* dir can be "left" or "right".
+*/
+function MoveList(board, listId, dir)
+{
+    //
+}
+
+/*
+* Pure function.
+* Returns a new board, with one new card at the end of the list with given listId.
+* Chooses default values for the card's properties.
+*/
+function AddCard(board, listId)
+{
+    //
+}
+
+/*
+* Pure function.
+* Returns a new board, with one new list in rightmost position.
+* Chooses default values for the card's properties.
+*/
+function AddList(board)
+{
+    //
+}
+
+/*
+* Pure function.
+* Returns a new board, with the card with given cardId removed.
+*/
+function DeleteCard(board, cardId)
+{
+    //
+}
+
+/*
+* Pure function.
+* Returns a new board, with the list with given listId removed.
+*/
+function DeleteList(board, listId)
+{
+    //
+}
+
+/*
+* Pure function.
+* Returns a new board, where the card with supplied cardId is assigned
+* the new title newTitle.
+*/
+function RenameCard(board, cardId, newTitle)
+{
+    //
+}
+
+/*
+* Pure function.
+* Returns a new board, where the list with supplied listId is assigned
+* the new title newTitle.
+*/
+function RenameList(board, listId, newTitle)
+{
+    //
+}
+
+/*
+* Pure function.
+* Returns a new board, where the card with supplied cardId is assigned
+* the new string array of labels newLabels.
+* Can be used for adding/deleting/renaming a label.
+* Chooses a default name for new labels.
+*/
+function ChangeCardLabels(board, cardId, newLabels)
+{
+    //
+}
+
+/*
+* Pure function.
+* Returns a new board, where the card with supplied cardId is assigned
+* the new notes string newNote.
+*/
+function ChangeCardNotes(board, cardId, newNotes)
+{
+    //
+}
+
+/*
+*
+*/
+function f()
+{
+    //
 }
