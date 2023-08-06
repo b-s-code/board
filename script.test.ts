7d3a2685130e6268b8813fdb431f7d17a0578417
b-s-code <89502375+b-s-code@users.noreply.github.com>
2023-08-06 20:59:17 +0800

    clarify expected behaviour etc.

--- script.test.ts
+++ script.test.ts
@@ -20,14 +20,19 @@ describe('Card movement', () =>
   {
     // Need to use spread operator to get a copy of
     // SampleBoardState, else it's vulnerable to
-    // mutation since objects are references in JS.
+    // mutation since objects are reference types in JS.
     var expectedOutput: BoardState = {...SampleBoardState};
+
+    // This approach is only valid because
+    // SampleBoardState has an ascending
+    // sequnce as the value of listPositions...
     expectedOutput.listsCards = 
     [
         [0, 1],
         [2, 3], /* Have decided when a card is moved
-                   to a new list, it should be made
-                   the final card. */
+                   to a new list, it should be moved
+                   to same vert. pos. if possible, else
+                   made the final card. */
         [4],
         [5, 6, 7]
     ];
